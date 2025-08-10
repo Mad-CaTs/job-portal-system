141234ebce9a63a2d96184825e1ec758feb17880
Mad-CaTs <markusperezch1@gmail.com>
2025-08-10 01:35:25 -0500

    controlador usuario

--- auth-service/adapter/src/main/java/com/miportal/authservice/adapter/in/rest/UsuarioController.java
+++ auth-service/adapter/src/main/java/com/miportal/authservice/adapter/in/rest/UsuarioController.java
@@ -1,5 +1,29 @@
 package com.miportal.authservice.adapter.in.rest;
 
+import com.miportal.authservice.application.dto.usuario.UsuarioCreateRequest;
+import com.miportal.authservice.application.dto.usuario.UsuarioDTO;
+import com.miportal.authservice.application.in.service.UsuarioService;
+import com.miportal.authservice.model.usuario.Usuario;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 @RestController
+@RequestMapping("/api/usuarios")
+@RequiredArgsConstructor
 public class UsuarioController {
+
+    private final UsuarioService usuarioService;
+
+    @PostMapping
+    public ResponseEntity<UsuarioDTO> crearUsuario(@RequestBody @Valid UsuarioCreateRequest request) {
+        UsuarioDTO usuario = usuarioService.crearUsuario(request);
+        return ResponseEntity.status(HttpStatus.CREATED).body(usuario);
+    }
+
 }
